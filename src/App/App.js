f0e70f125a936a239c3fcdbdaf0c2508ccb4d6fc
jay_wu <jay_wu>
2021-03-27 23:07:36 +0800

    add navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { Home, Books, Electronics } from "../pages";
+import { Navbar } from '../components';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
+        <Navbar />
+        
         <Route path="/" exact component={Home} />
         <Route path="/books" component={Books} />
         <Route path="/electronics" component={Electronics} />
